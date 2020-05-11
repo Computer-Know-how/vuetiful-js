1056158f9728bd3579a6adfd64b76db75f49c994
haevenshane <haevenshane@gmail.com>
2020-05-11 16:36:13 -0500

    removed unnecessary dynamic styling change

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -906,7 +906,6 @@ export default {
 			const row = document.querySelector(`#table__row-${rowIndex}`);
 			row.style.height = `${cell.data.length*40}px`;
 			row.children[1].children[0].style.display = 'none';
-			row.children[8].children[0].style.marginTop = `${(cell.data.length-1)*20}px`;
 			document.querySelector(`#table__cell-${cellIndex}`).style.overflowY = 'hidden';
 
 			return;
