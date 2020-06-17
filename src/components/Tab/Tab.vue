d20ede6a4dbfbc9e424ba4837fe50b3feeb318c0
Jeremy Johnson <jeremy@compknowhow.com>
2020-06-17 08:11:00 -0500

    Added new tabs component

--- src/components/Tab/Tab.vue
+++ src/components/Tab/Tab.vue
@@ -1,21 +1,22 @@
 <template>
-	<div class='tab' v-show='isActive'>
-		<slot></slot>
+	<div class="radiogroup --fade-in">
+		<ul v-for="(r, index) in this.labels" :key="(r, index)">
+			<li>
+				<input type="radio" :name="r.name" :id="r.id" :v-model="r.model" :value="r.value" v-on:click="(r.func)(r.value)" :checked="r.checked"/>
+				<label :for="r.value">{{ r.label }}</label>
+				<span class="check"></span>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
 export default {
 	props: {
-		title: {
-			type: String,
-			default: 'Tab'
-		}
+		labels: Array
 	},
 	data () {
-		return {
-			isActive: true
-		};
+		return {};
 	}
 };
 </script>
