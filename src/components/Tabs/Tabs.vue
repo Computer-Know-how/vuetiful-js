498ceb15bfa8dbf47f1fe39b0df8aa99ad9e578e
Jeremy Johnson <jeremy@compknowhow.com>
2020-06-19 11:38:45 -0500

    Refactor

--- src/components/Tabs/Tabs.vue
+++ src/components/Tabs/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="radiogroup --fade-in">
-		<ul v-for="(r, index) in this.labels" :key="(r, index)">
-			<li>
+		<ul>
+			<li v-for="(r, index) in this.labels" :key="(r, index)">
 				<input type="radio" :name="r.name" :id="r.id" :v-model="r.model" :value="r.value" @click="(r.func)(r.value)" :checked="r.checked"/>
 				<label :for="r.value">{{ r.label }}</label>
 				<span class="check"></span>
