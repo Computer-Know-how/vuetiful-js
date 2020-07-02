f17c587aa8e663eb780173f1e4014143d4884300
Abram Hurlbert <abram@compknowhow.com>
2020-07-02 12:13:55 -0500

    CDH-1689_CDH-1696: Adding styling for alertPopup and button icons

--- src/components/AlertPopUp/AlertPopUp.vue
+++ src/components/AlertPopUp/AlertPopUp.vue
@@ -1,10 +1,12 @@
 <template>
-	<div class="popup__container">
-		<div class="alert-popup box-shadow">
-			<img class="popup__img--alert" src="https://upload.wikimedia.org/wikipedia/commons/9/99/OOjs_UI_icon_alert-yellow.svg" />
-			<p class="popup__message--alert">{{ alertMessage }}</p>
-			<button class="btn popup__btn--alert">{{ topButton }}</button>
-			<button class="btn popup__btn--alert btn --secondary">{{ bottomButton }}</button>
+	<div class="popup__backdrop">
+		<div class="popup__container">
+			<div class="alert-popup box-shadow">
+				<img class="popup__img--alert" src="https://upload.wikimedia.org/wikipedia/commons/9/99/OOjs_UI_icon_alert-yellow.svg" />
+				<p class="popup__message--alert">{{ alertMessage }}</p>
+				<button class="btn popup__btn--alert" @click="topBtn">{{ topButton }}</button>
+				<button class="btn popup__btn--alert btn --secondary" @click="bottomBtn">{{ bottomButton }}</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -13,7 +15,14 @@
 export default {
 	name: 'vuetiful-alert-popup',
 	components: {},
-	methods: {},
+	methods: {
+		topBtn() {
+			this.$emit('topBtn');
+		},
+		bottomBtn() {
+			this.$emit('bottomBtn');
+		}
+	},
 	props: {
 		alertMessage: String,
 		topButton: String,
